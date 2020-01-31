ec9f8166960c67aa33201aab6bc8f4c4612418b3
Ângelo Freitas Santos <ajkfreitas@gmail.com>
2020-01-31 10:28:04 -0300

    Posição xadrez

---
+++ Xadrez-Console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xadrez_Console.tabuleiro;
+
+namespace xadrez
+{
+    public class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+        public Posicao ToPosition()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+        public override string ToString()
+        {
+            return $"{Coluna} {Linha}";
+        }
+    }
+}
